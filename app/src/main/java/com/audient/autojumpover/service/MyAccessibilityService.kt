ae672929d9f6a69535a350b243d2f149a63ff047
林泉 <591928179@qq.com>
2020-10-10 17:20:52 +0800

    还是去掉未适配的应用自动检测吧，各种误触

--- app/src/main/java/com/audient/autojumpover/service/MyAccessibilityService.kt
+++ app/src/main/java/com/audient/autojumpover/service/MyAccessibilityService.kt
@@ -23,6 +23,12 @@ class MyAccessibilityService : AccessibilityService() {
         when (event?.eventType) {
             AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED -> {
 //                Log.i(TAG, "TYPE_WINDOW_CONTENT_CHANGED -> ${event.packageName}/${event.className}")
+
+                // TODO: 2020/10/10 需不需要排除应用的功能：列出所有应用，让用户选择
+                // 排除的应用
+//                val excludePackages = arrayOf("com.audient.autojumpover")
+//                if (excludePackages.contains(event.packageName)) return
+
                 when (event.packageName) {
                     "com.ximalaya.ting.android" -> {
                         val app = "喜马拉雅"
@@ -32,7 +38,7 @@ class MyAccessibilityService : AccessibilityService() {
 //                            LogUtils.d(TAG, "没有找到对应的控件[$app][${event.className}]")
                                 return
                             }
-                        jump(node, app)
+                        jump(node, app, event.packageName.toString())
                     }
                     "com.hskj.palmmetro" -> {
                         val app = "玩转地铁"
@@ -42,22 +48,23 @@ class MyAccessibilityService : AccessibilityService() {
 //                            LogUtils.d(TAG, "没有找到对应的控件[$app][${event.className}]")
                                 return
                             }
-                        jump(node, app)
+                        jump(node, app, event.packageName.toString())
                     }
-                    else -> {
-                        val app = "未知应用"
-                        val node = rootInActiveWindow?.findAccessibilityNodeInfosByText("跳过")
-                            ?.getOrNull(0) ?: run {
-                            return
-                        }
-                        jump(node, app)
-                    }
+                    // TODO: 2020/10/10 不要这个了，各种误触发
+//                    else -> {
+//                        val app = "未知应用"
+//                        val node = rootInActiveWindow?.findAccessibilityNodeInfosByText("跳过")
+//                            ?.getOrNull(0) ?: run {
+//                            return
+//                        }
+//                        jump(node, app, event.packageName.toString())
+//                    }
                 }
             }
         }
     }
 
-    private fun jump(node: AccessibilityNodeInfo, app: String) {
+    private fun jump(node: AccessibilityNodeInfo, app: String, packageName: String) {
         var success = node.performAction(AccessibilityNodeInfo.ACTION_CLICK)
 
         // 如果失败，尝试点击父控件
@@ -66,7 +73,7 @@ class MyAccessibilityService : AccessibilityService() {
         }
 
         ToastUtils.show("$app:自动跳过[$success]")
-        Log.i(TAG, "$app:自动跳过[$success]")
+        Log.i(TAG, "$app:自动跳过[$success][$packageName]")
     }
 
     private fun AccessibilityNodeInfo.traversal() {
