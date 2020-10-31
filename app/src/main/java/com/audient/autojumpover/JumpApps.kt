da7a255164682eb3e825dfb6315f3d9048a68c24
林泉 <591928179@qq.com>
2020-10-31 18:18:57 +0800

    适配美团外卖

--- app/src/main/java/com/audient/autojumpover/JumpApps.kt
+++ app/src/main/java/com/audient/autojumpover/JumpApps.kt
@@ -15,19 +15,38 @@ object JumpApps {
         windowStateChangedAppList.associateBy { jumpApp: JumpApp -> jumpApp.packageName }
     }
 
-    private val windowContentChangedAppList: ArrayList<JumpApp> by lazy {
-        arrayListOf(
-            JumpApp("玩转地铁", "com.hskj.palmmetro", "com.hskj.palmmetro:id/tt_splash_skip_btn"),
-            JumpApp("咪咕音乐", "cmccwm.mobilemusic", "cmccwm.mobilemusic:id/bt_skip_ad")
-        )
-    }
-
+    // 先加到windowStateChangedAppList，如果不起作用，再加到windowContentChangedAppList
     private val windowStateChangedAppList: ArrayList<JumpApp> by lazy {
         arrayListOf(
             JumpApp(
                 "喜马拉雅",
                 "com.ximalaya.ting.android",
                 "com.ximalaya.ting.android:id/host_count_down_click_lay"
+//            ),
+//            JumpApp(
+//                "美团外卖",
+//                "com.sankuai.meituan.takeoutnew",
+//                "com.sankuai.meituan.takeoutnew:id/ll_skip"
+            )
+        )
+    }
+
+    private val windowContentChangedAppList: ArrayList<JumpApp> by lazy {
+        arrayListOf(
+            JumpApp(
+                "玩转地铁",
+                "com.hskj.palmmetro",
+                "com.hskj.palmmetro:id/tt_splash_skip_btn"
+            ),
+            JumpApp(
+                "咪咕音乐",
+                "cmccwm.mobilemusic",
+                "cmccwm.mobilemusic:id/bt_skip_ad"
+            ),
+            JumpApp(
+                "美团外卖",
+                "com.sankuai.meituan.takeoutnew",
+                "com.sankuai.meituan.takeoutnew:id/ll_skip"
             )
         )
     }
